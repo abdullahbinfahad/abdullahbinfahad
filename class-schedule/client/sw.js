80938b7c90a43606fda2e87ba76142171a135265
Abdullah Bin Fahad <abdullahbinfahad.abf@gmail.com>
2026-08-15 16:40:25 +0800

    Add files via upload

---
+++ class-schedule/client/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clients => {
+    const openClient = clients.find(client => 'focus' in client);
+    return openClient ? openClient.focus() : self.clients.openWindow(self.registration.scope + 'index.html');
+  }));
+});
